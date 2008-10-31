aa579b7377a457a7b452183345668bbebee68e07
Chris Heald <cheald@gmail.com>
2008-10-30 22:04:36 -0700

    Add zhTW and zhCN localization

---
+++ localization/zhCN.lua
@@ -0,0 +1,118 @@
+﻿-- This file is script-generated and should not be manually edited. 
+-- Localizers may copy this file to edit as necessary. 
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0") 
+local L = AceLocale:NewLocale("SexyMap", "zhCN", false) 
+if not L then return end 
+ 
+-- ./AutoZoom.lua
+L["Autozoom out after..."] = "自动缩放到最小"
+L["Number of seconds to autozoom out after. Set to 0 to turn off Autozoom."] = "几秒后自动缩放到最小。‘0’为不自动缩放"
+
+-- ./BorderPresets.lua
+-- no localization
+
+-- ./Borders.lua
+L["1. Background"] = "1、背景"
+L["2. Border"] = "2、边框"
+L["3. Artwork"] = "3、艺术装饰"
+L["4. Overlay"] = "4、覆盖"
+L["5. Highlight"] = "5、高亮"
+L["Blend (normal)"] = "混合效果(普通)"
+L["Disable (opaque)"] = "禁用(不透明)"
+L["Alpha Key (1-bit alpha)"] = "测试键(1位测试)"
+L["Mod Blend (modulative)"] = "模块混合(模块式)"
+L["Add Blend (additive)"] = "添加混合(附加式)"
+L["Hide default border"] = "隐藏默认边框"
+L["Hide the default border on the minimap."] = "隐藏小地图默认的边框。"
+L["Current Borders"] = "当前边框"
+L["Enter a name to create a new border. The name can be anything you like to help you identify that border."] = "输入一个名称以创建一个新的边框。可以用任何你想用来帮助你识别此边框的名称。"
+L["Create new border"] = "创建一个新的边框"
+L["Clear & start over"] = "重置"
+L["Clear the current borders and start fresh"] = "清除当前边框重新开始设置"
+L["Borders"] = "边框"
+L["Preset"] = "预设"
+L["Select preset to load"] = "选择加载预设配置"
+L["Select a preset to load settings from. This will erase any of your current borders."] = "选择一个预设模板。这样做会清除所有你当前正在使用的边框。"
+L["This will wipe out any current settings!"] = "这样做将清除所有当前设置！"
+L["Delete"] = "删除"
+L["Really delete this preset? This can't be undone."] = "确定删除此预设效果？此操作将无法被恢复"
+L["Save current settings as preset..."] = "将当前设置保存为预设配置"
+L["Entry options"] = "基本设置"
+L["Name"] = "名称"
+L["Really delete this border?"] = "确定删除此边框效果？"
+L["Texture path"] = "纹理路径"
+L["Enter the full path to a texture to use. It's recommended that you use something like |cffff6600TexBrowser|r to find textures to use."] = "输入完整的纹理路径才能使用。建议你使用类似|cffff6600纹理浏览器|r的功能来找到需要使用纹理效果。"
+L["Open TexBrowser"] = "打开纹理浏览器"
+L["TexBrowser Not Installed"] = "没有安装纹理浏览器"
+L["Texture options"] = "纹理效果设置"
+L["Scale"] = "缩放"
+L["Rotation Speed"] = "旋转速度"
+L["Speed to rotate the texture at. A setting of 0 turns off rotation."] = "设置纹理效果的旋转速度。‘0’为不旋转"
+L["Static Rotation"] = "固定旋转"
+L["A static amount to rotate the texture by."] = "纹理效果旋转的定量"
+L["Match player rotation"] = "根据人物动作旋转"
+L["Normal rotation"] = "正向旋转"
+L["Reverse rotation"] = "反向旋转"
+L["Do not match player rotation"] = "不根据人物动作旋转"
+L["Texture tint"] = "纹理着色"
+L["Horizontal nudge"] = "水平位移"
+L["Vertical nudge"] = "垂直位移"
+L["Layer"] = "层级"
+L["Blend Mode"] = "混合模式"
+L["Disable Rotation"] = "禁用旋转"
+L["Force a square texture. Fixed distortion on square textures."] = "强制填充一个方形纹理。修正方形纹理上的变形失真"
+
+-- ./Buttons.lua
+L["Drag Radius"] = "拖动半径"
+L["Calendar"] = "日历"
+L["Map Button"] = "大地图按钮"
+L["Tracking Button"] = "追踪按钮"
+L["Zoom Buttons"] = "小地图缩放按钮"
+L["Clock"] = "时钟"
+L["Close button"] = "小地图开关按钮"
+L["Compass labels"] = "指南针"
+L["New mail indicator"] = "新邮件提示"
+L["Voice chat"] = "语音聊天按钮"
+L["Battlegrounds icon"] = "战场徽标"
+L["Always"] = "总是"
+L["Never"] = "从不"
+L["On hover"] = "鼠标停留时"
+
+-- ./General.lua
+L["Lock minimap"] = "锁定小地图"
+L["Show movers"] = "显示锚点"
+L["Clamp to screen"] = "固定在屏幕上"
+L["Opacity"] = "透明度"
+L["Minimap shape"] = "小地图形状"
+L["Right click map to configure"] = "右击小地图打开设置选项"
+
+-- ./moduleTemplate.lua
+-- no localization
+
+-- ./oldBorders.lua
+-- no localization
+
+-- ./Ping.lua
+L["Show who pinged"] = "显示ping值"
+
+-- ./SexyMap.lua
+L["Profiles"] = "配置文件"
+
+-- ./ZoneText.lua
+L["Horizontal position"] = "水平位置"
+L["Vertical position"] = "垂直位置"
+L["Width"] = "宽度"
+L["Background color"] = "背景颜色"
+L["Border color"] = "边框颜色"
+L["Font"] = "字体"
+L["Font Size"] = "字体大小"
+L["Font color"] = "字体颜色"
+
+-- ./localization/enUS.lua
+-- no localization
+
+-- ./localization/zhCN.lua
+-- no localization
+
+-- ./localization/zhTW.lua
+-- no localization
---
+++ localization/zhTW.lua
@@ -0,0 +1,118 @@
+﻿-- This file is script-generated and should not be manually edited. 
+-- Localizers may copy this file to edit as necessary. 
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0") 
+local L = AceLocale:NewLocale("SexyMap", "zhTW", false) 
+if not L then return end 
+ 
+-- ./AutoZoom.lua
+L["Autozoom out after..."] = "自動縮放到最小"
+L["Number of seconds to autozoom out after. Set to 0 to turn off Autozoom."] = "幾秒後自動縮放到最小。‘0’為不自動縮放"
+
+-- ./BorderPresets.lua
+-- no localization
+
+-- ./Borders.lua
+L["1. Background"] = "1、背景"
+L["2. Border"] = "2、邊框"
+L["3. Artwork"] = "3、藝術裝飾"
+L["4. Overlay"] = "4、覆蓋"
+L["5. Highlight"] = "5、高亮"
+L["Blend (normal)"] = "混合效果(普通)"
+L["Disable (opaque)"] = "禁用(不透明)"
+L["Alpha Key (1-bit alpha)"] = "測試鍵(1位測試)"
+L["Mod Blend (modulative)"] = "模塊混合(模塊式)"
+L["Add Blend (additive)"] = "添加混合(附加式)"
+L["Hide default border"] = "隱藏默認邊框"
+L["Hide the default border on the minimap."] = "隱藏小地圖默認的邊框。"
+L["Current Borders"] = "當前邊框"
+L["Enter a name to create a new border. The name can be anything you like to help you identify that border."] = "輸入一個名稱以創建一個新的邊框。可以用任何你想用來幫助你識別此邊框的名稱。"
+L["Create new border"] = "創建一個新的邊框"
+L["Clear & start over"] = "重置"
+L["Clear the current borders and start fresh"] = "清除當前邊框重新開始設置"
+L["Borders"] = "邊框"
+L["Preset"] = "預設"
+L["Select preset to load"] = "選擇加載預設配置"
+L["Select a preset to load settings from. This will erase any of your current borders."] = "選擇一個預設模板。這樣做會清除所有你當前正在使用的邊框。"
+L["This will wipe out any current settings!"] = "這樣做將清除所有當前設置！"
+L["Delete"] = "刪除"
+L["Really delete this preset? This can't be undone."] = "確定刪除此預設效果？此操作將無法被恢複"
+L["Save current settings as preset..."] = "將當前設置保存為預設配置"
+L["Entry options"] = "基本設置"
+L["Name"] = "名稱"
+L["Really delete this border?"] = "確定刪除此邊框效果？"
+L["Texture path"] = "紋理路徑"
+L["Enter the full path to a texture to use. It's recommended that you use something like |cffff6600TexBrowser|r to find textures to use."] = "輸入完整的紋理路徑才能使用。建議你使用類似|cffff6600紋理瀏覽器|r的功能來找到需要使用紋理效果。"
+L["Open TexBrowser"] = "打開紋理瀏覽器"
+L["TexBrowser Not Installed"] = "沒有安裝紋理瀏覽器"
+L["Texture options"] = "紋理效果設置"
+L["Scale"] = "縮放"
+L["Rotation Speed"] = "旋轉速度"
+L["Speed to rotate the texture at. A setting of 0 turns off rotation."] = "設置紋理效果的旋轉速度。‘0’為不旋轉"
+L["Static Rotation"] = "固定旋轉"
+L["A static amount to rotate the texture by."] = "紋理效果旋轉的定量"
+L["Match player rotation"] = "根據人物動作旋轉"
+L["Normal rotation"] = "正向旋轉"
+L["Reverse rotation"] = "反向旋轉"
+L["Do not match player rotation"] = "不根據人物動作旋轉"
+L["Texture tint"] = "紋理著色"
+L["Horizontal nudge"] = "水平位移"
+L["Vertical nudge"] = "垂直位移"
+L["Layer"] = "層級"
+L["Blend Mode"] = "混合模式"
+L["Disable Rotation"] = "禁用旋轉"
+L["Force a square texture. Fixed distortion on square textures."] = "強制填充一個方形紋理。修正方形紋理上的變形失真"
+
+-- ./Buttons.lua
+L["Drag Radius"] = "拖動半徑"
+L["Calendar"] = "日曆"
+L["Map Button"] = "大地圖按鈕"
+L["Tracking Button"] = "追蹤按鈕"
+L["Zoom Buttons"] = "小地圖縮放按鈕"
+L["Clock"] = "時鐘"
+L["Close button"] = "小地圖開關按鈕"
+L["Compass labels"] = "指南針"
+L["New mail indicator"] = "新郵件提示"
+L["Voice chat"] = "語音聊天按鈕"
+L["Battlegrounds icon"] = "戰場徽標"
+L["Always"] = "總是"
+L["Never"] = "從不"
+L["On hover"] = "鼠標停留時"
+
+-- ./General.lua
+L["Lock minimap"] = "鎖定小地圖"
+L["Show movers"] = "顯示錨點"
+L["Clamp to screen"] = "固定在屏幕上"
+L["Opacity"] = "透明度"
+L["Minimap shape"] = "小地圖形狀"
+L["Right click map to configure"] = "右擊小地圖打開設置選項"
+
+-- ./moduleTemplate.lua
+-- no localization
+
+-- ./oldBorders.lua
+-- no localization
+
+-- ./Ping.lua
+L["Show who pinged"] = "顯示ping值"
+
+-- ./SexyMap.lua
+L["Profiles"] = "配置文件"
+
+-- ./ZoneText.lua
+L["Horizontal position"] = "水平位置"
+L["Vertical position"] = "垂直位置"
+L["Width"] = "寬度"
+L["Background color"] = "背景顏色"
+L["Border color"] = "邊框顏色"
+L["Font"] = "字體"
+L["Font Size"] = "字體大小"
+L["Font color"] = "字體顏色"
+
+-- ./localization/enUS.lua
+-- no localization
+
+-- ./localization/zhCN.lua
+-- no localization
+
+-- ./localization/zhTW.lua
+-- no localization
