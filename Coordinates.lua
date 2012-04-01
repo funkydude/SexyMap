1223111a6331b530bc06dcbb9a04dfcd4c81c50e
funkydude <funkydude@users.noreply.github.com>
2012-04-01 01:58:44 +0100

    Cleanup & Optimize Coordinates module

--- Coordinates.lua
+++ Coordinates.lua
@@ -4,23 +4,22 @@ local parent = addon.SexyMap
 local modName = "Coordinates"
 local mod = addon.SexyMap:NewModule(modName, "AceTimer-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("SexyMap")
-local db
 
 local options = {
 	type = "group",
 	name = L["Coordinates"],
 	childGroups = "tab",
-	disabled = function() return not db.enabled end,
+	disabled = function() return not mod.db.profile.enabled end,
 	args = {
 		enable = {
 			type = "toggle",
 			name = L["Enable Coordinates"],
 			order = 1,
 			get = function()
-				return db.enabled
+				return mod.db.profile.enabled
 			end,
 			set = function(info, v)
-				db.enabled = v
+				mod.db.profile.enabled = v
 				if v then
 					parent:EnableModule(modName)
 				else
@@ -34,10 +33,10 @@ local options = {
 			name = L["Lock coordinates"],
 			order = 2,
 			get = function()
-				return db.locked
+				return mod.db.profile.locked
 			end,
 			set = function(info, v)
-				db.locked = v
+				mod.db.profile.locked = v
 			end,
 			width = "full",
 		},
@@ -50,10 +49,10 @@ local options = {
 			step = 1,
 			bigStep = 1,
 			get = function()
-				return db.fontSize or 12
+				return mod.db.profile.fontSize or 12
 			end,
 			set = function(info, v)
-				db.fontSize = v
+				mod.db.profile.fontSize = v
 				mod:Update()
 				mod:UpdateCoords()
 			end
@@ -64,12 +63,12 @@ local options = {
 			order = 4,
 			hasAlpha = true,
 			get = function()
-				local c = db.fontColor
+				local c = mod.db.profile.fontColor
 				local r, g, b, a = c.r or 0, c.g or 0, c.b or 0, c.a or 1
 				return r, g, b, a
 			end,
 			set = function(info, r, g, b, a)
-				local c = db.fontColor
+				local c = mod.db.profile.fontColor
 				c.r, c.g, c.b, c.a = r, g, b, a
 				mod:Update()
 			end
@@ -80,12 +79,12 @@ local options = {
 			order = 5,
 			hasAlpha = true,
 			get = function()
-				local c = db.backgroundColor
+				local c = mod.db.profile.backgroundColor
 				local r, g, b, a = c.r or 0, c.g or 0, c.b or 0, c.a or 1
 				return r, g, b, a
 			end,
 			set = function(info, r, g, b, a)
-				local c = db.backgroundColor
+				local c = mod.db.profile.backgroundColor
 				c.r, c.g, c.b, c.a = r, g, b, a
 				mod:Update()
 			end
@@ -96,12 +95,12 @@ local options = {
 			order = 5,
 			hasAlpha = true,
 			get = function()
-				local c = db.borderColor
+				local c = mod.db.profile.borderColor
 				local r, g, b, a = c.r or 0, c.g or 0, c.b or 0, c.a or 1
 				return r, g, b, a
 			end,
 			set = function(info, r, g, b, a)
-				local c = db.borderColor
+				local c = mod.db.profile.borderColor
 				c.r, c.g, c.b, c.a = r, g, b, a
 				mod:Update()
 			end
@@ -117,25 +116,6 @@ local options = {
 	}
 }
 
-local function start(self)
-	if db.locked then return end
-	self:StartMoving()
-	self.moving = true
-end
-
-local function finish(self)
-	if not self.moving then return end
-	self.moving = nil
-	self:StopMovingOrSizing()
-	local x, y = self:GetCenter()
-	local mx, my = Minimap:GetCenter()
-	local dx, dy = mx - x, my - y
-	self:ClearAllPoints()
-	self:SetPoint("CENTER", Minimap, "CENTER", -dx, -dy)
-	db.x = dx
-	db.y = dy
-end
-
 local defaults = {
 	profile = {
 		borderColor = {},
@@ -145,106 +125,114 @@ local defaults = {
 		enabled = false
 	}
 }
-local coordFrame, xcoords, ycoords
+
 function mod:OnInitialize()
 	self.db = parent.db:RegisterNamespace(modName, defaults)
-	db = self.db.profile
-
-	coordFrame = CreateFrame("Frame", "SexyMapCoordFrame", Minimap)
-	coordFrame:SetBackdrop({
-		bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",
-		edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
-		insets = {left = 2, top = 2, right = 2, bottom = 2},
-		edgeSize = 12,
-		tile = true
-	})
-	xcoords = coordFrame:CreateFontString(nil, nil, "GameFontNormalSmall")
-	xcoords:SetPoint("TOPLEFT", coordFrame, "TOPLEFT", 5, 0)
-	xcoords:SetPoint("BOTTOMRIGHT")
-	xcoords:SetPoint("TOPRIGHT", coordFrame, "TOP")
-	xcoords:SetJustifyH("LEFT")
-
-	ycoords = coordFrame:CreateFontString(nil, nil, "GameFontNormalSmall")
-	ycoords:SetPoint("TOPLEFT", xcoords, "TOPRIGHT", 4, 0)
-	ycoords:SetPoint("BOTTOMLEFT", xcoords, "BOTTOMRIGHT")
-	ycoords:SetPoint("TOPRIGHT", coordFrame, "TOPRIGHT", -3, 0)
-	ycoords:SetPoint("BOTTOMRIGHT")
-	ycoords:SetJustifyH("LEFT")
-
-	coordFrame:SetMovable(true)
-	coordFrame:EnableMouse()
-	coordFrame.sexyMapIgnore = true
-
-	coordFrame:SetScript("OnMouseDown", start)
-	coordFrame:SetScript("OnMouseUp", finish)
-
-	self:UpdateCoords()
-	self:Update()
 	parent:RegisterModuleOptions(modName, options, modName)
 end
 
+local coordFrame, coordsText
 function mod:OnEnable()
-	db = self.db.profile
-	if not db.enabled then
+	if not self.db.profile.enabled then
 		parent:DisableModule(modName)
 		return
 	end
-	if db.x then
+
+	if not coordFrame then
+		coordFrame = CreateFrame("Frame", "SexyMapCoordFrame", Minimap)
+		coordFrame:SetBackdrop({
+			bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",
+			edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+			insets = {left = 2, top = 2, right = 2, bottom = 2},
+			edgeSize = 12,
+			tile = true
+		})
+		coordsText = coordFrame:CreateFontString(nil, nil, "GameFontNormalSmall")
+		coordsText:SetPoint("CENTER", coordFrame, "CENTER")
+		coordsText:SetJustifyH("CENTER")
+
+		coordFrame:SetMovable(true)
+		coordFrame:EnableMouse()
+		coordFrame.sexyMapIgnore = true
+
+		coordFrame:SetScript("OnMouseDown", function(self)
+			if not mod.db.profile.locked then
+				self:StartMoving()
+				self.moving = true
+			end
+		end)
+		coordFrame:SetScript("OnMouseUp", function(self)
+			if self.moving then
+				self.moving = nil
+				self:StopMovingOrSizing()
+				local x, y = self:GetCenter()
+				local mx, my = Minimap:GetCenter()
+				local dx, dy = mx - x, my - y
+				self:ClearAllPoints()
+				self:SetPoint("CENTER", Minimap, "CENTER", -dx, -dy)
+				mod.db.profile.x = dx
+				mod.db.profile.y = dy
+			end
+		end)
+
+		self:UpdateCoords()
+		self:Update()
+	end
+	if self.db.profile.x then
 		coordFrame:ClearAllPoints()
-		coordFrame:SetPoint("CENTER", Minimap, "CENTER", -db.x, -db.y)
+		coordFrame:SetPoint("CENTER", Minimap, "CENTER", -self.db.profile.x, -self.db.profile.y)
 	else
 		coordFrame:SetPoint("CENTER", Minimap, "BOTTOM")
 	end
 
 	coordFrame:Show()
-	self.updateTimer = self:ScheduleRepeatingTimer("UpdateCoords", 0.05)
-	-- parent:GetModule("Buttons"):MakeMovable(coordFrame)
+	self:ScheduleRepeatingTimer("UpdateCoords", 0.2)
 end
 
 function mod:OnDisable()
-	self:CancelTimer(self.updateTimer, true)
-	coordFrame:Hide()
+	if coordFrame then
+		self:CancelAllTimers()
+		coordFrame:Hide()
+	end
 end
 
-function mod:UpdateCoords()
-	local x, y = GetPlayerMapPosition("player")
-	xcoords:SetText(("%2.1f, "):format(x*100))
-	ycoords:SetText(("%2.1f"):format(y*100))
-	-- , %2.1f"):format(x*100,y*100))
+do
+	local GetPlayerMapPosition = GetPlayerMapPosition
+	local txt = "%.1f, %.1f"
+	function mod:UpdateCoords()
+		local x, y = GetPlayerMapPosition"player"
+		coordsText:SetFormattedText(txt, x*100, y*100)
+	end
 end
 
 function mod:Update()
-	if db.borderColor then
-		local c = db.borderColor
+	if self.db.profile.borderColor then
+		local c = self.db.profile.borderColor
 		coordFrame:SetBackdropBorderColor(c.r or 0, c.g or 0, c.b or 0, c.a or 1)
 	end
 
-	if db.backgroundColor then
-		local c = db.backgroundColor
+	if self.db.profile.backgroundColor then
+		local c = self.db.profile.backgroundColor
 		coordFrame:SetBackdropColor(c.r or 0, c.g or 0, c.b or 0, c.a or 1)
 	end
 
-	if db.fontColor then
-		local c = db.fontColor
-		xcoords:SetTextColor(c.r or 1, c.g or 1, c.b or 1, c.a or 1)
-		ycoords:SetTextColor(c.r or 1, c.g or 1, c.b or 1, c.a or 1)
+	if self.db.profile.fontColor then
+		local c = self.db.profile.fontColor
+		coordsText:SetTextColor(c.r or 1, c.g or 1, c.b or 1, c.a or 1)
 	end
 
-	if db.fontSize then
-		local f, s, flags = xcoords:GetFont()
-		xcoords:SetFont(f, db.fontSize, flags)
-		ycoords:SetFont(f, db.fontSize, flags)
+	if self.db.profile.fontSize then
+		local f, s, flags = coordsText:GetFont()
+		coordsText:SetFont(f, self.db.profile.fontSize, flags)
 	end
 
-	local pt = xcoords:GetText()
-	xcoords:SetText(("%2.1f,  %2.1f"):format(22.222,22.222))
-	coordFrame:SetWidth(xcoords:GetStringWidth() * 1.2)
-	coordFrame:SetHeight(xcoords:GetStringHeight() + 10)
-	xcoords:SetText(pt)
+	coordFrame:SetWidth(coordsText:GetStringWidth() * 1.2)
+	coordFrame:SetHeight(coordsText:GetStringHeight() + 10)
 end
 
 function mod:ResetPosition()
 	coordFrame:ClearAllPoints()
 	coordFrame:SetPoint("CENTER", Minimap, "BOTTOM")
-	db.x, db.y = nil, nil
+	self.db.profile.x, self.db.profile.y = nil, nil
 end
+
