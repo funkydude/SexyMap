be2d47455456ef66559da7efc31575559fd075fe
funkydude <funkydude@users.noreply.github.com>
2012-08-21 20:23:45 +0100

    missed ptBR

---
+++ localization/ptBR.lua
@@ -0,0 +1,6 @@
+
+if GetLocale() ~= "ptBR" then return end
+local _, sm = ...
+local L = sm.L
+--@localization(locale="ptBR", format="lua_additive_table", handle-unlocalized="ignore", table-name="L")@
+
