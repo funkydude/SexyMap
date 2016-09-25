319bab6279684eb8d79af6ee1f8fd13a507a2249
funkydude <funkydude@users.noreply.github.com>
2016-09-25 15:01:28 +0100

    move locale to repo

--- localization/esMX.lua
+++
@@ -1,6 +0,0 @@
-
-if GetLocale() ~= "esMX" then return end
-local _, sm = ...
-local L = sm.L
---@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="comment", table-name="L")@
-
